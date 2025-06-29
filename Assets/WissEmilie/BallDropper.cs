e55d76fe22e81c706efa9c17ce7b95af4ec076d1
Tim <git@king-management.nl>
2025-06-29 16:19:30 +0200

    Allow ball drop

--- Assets/WissEmilie/BallDropper.cs
+++ Assets/WissEmilie/BallDropper.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class OVRControllerLogger : MonoBehaviour
 {
+    public GameObject volleyball;
+    
     void Update()
     {
         // Check for button presses and log all data
         if (OVRInput.GetDown(OVRInput.Button.One) || OVRInput.GetDown(OVRInput.Button.Two) ||
             OVRInput.GetDown(OVRInput.Button.Three) || OVRInput.GetDown(OVRInput.Button.Four) ||
-            OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) ||
-            OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger) || OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+            OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) ||
+            OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) ||
+            OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger) ||
+            OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
         {
-            GameObject volleyball = GameObject.Find("volleyball");
             if (volleyball != null)
             {
                 volleyball.SetActive(true);
@@ -26,4 +29,5 @@ public class OVRControllerLogger : MonoBehaviour
                 leftControllerObject.SetActive(false);
             }
         }
+    }
 }
